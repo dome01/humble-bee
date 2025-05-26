d03ecab64b70b110d3109c079623c29711712519
dome01 <hathanh1920@gmail.com>
2025-05-26 19:38:05 +0200

    Simple Python APIs with tests

---
+++ api2/main.py
@@ -0,0 +1,9 @@
+from flask import Flask # type: ignore
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from API 2!"  
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
