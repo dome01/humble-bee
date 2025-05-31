bcfe95baef826503b2401748ac45ada4f1bad2dc
dome01 <hathanh1920@gmail.com>
2025-05-31 16:51:30 +0200

    Convert to idgen & cleanup

--- idgen-api/test_main.py
+++ idgen-api/test_main.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.text == "Hello from API 2!"
+    assert response.text == "Hello from ID-Gen!"
 
 def test_create_id():
     response = client.post("/ids")
