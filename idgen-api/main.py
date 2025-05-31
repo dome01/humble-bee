e35c5ff298b30e4857d2eca5f075fa30dadbb42d
dome01 <hathanh1920@gmail.com>
2025-06-01 00:12:02 +0200

    Supabase integration

--- idgen-api/main.py
+++ idgen-api/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 from uuid import uuid4
+from dotenv import load_dotenv
+import os
+from supabase import create_client, Client
+
+
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 app = FastAPI()
 
@@ -11,4 +22,5 @@ def home():
 @app.post("/ids")
 def create_id():
     new_id = str(uuid4())
+    supabase.table("ids").insert({"id": new_id}).execute()
     return {"id": new_id}
